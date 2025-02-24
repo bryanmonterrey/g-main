acac153b43e0ce5d8483b783ecc870008f223c1a
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-02-23 21:22:08 -0500

    f

---
+++ tools/mayan/ERC20PermitABI.ts
@@ -0,0 +1,17 @@
+export const ERC20Permit_ABI = [
+  {
+    "inputs": [
+      { "name": "owner", "type": "address" },
+      { "name": "spender", "type": "address" },
+      { "name": "value", "type": "uint256" },
+      { "name": "deadline", "type": "uint256" },
+      { "name": "v", "type": "uint8" },
+      { "name": "r", "type": "bytes32" },
+      { "name": "s", "type": "bytes32" }
+    ],
+    "name": "permit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]; 
--- tools/mayan/swap.ts
+++ tools/mayan/swap.ts
@@ -19,7 +19,7 @@ import {
   TypedDataEncoder,
   Wallet,
 } from "ethers";
-import { abi as ERC20Permit_ABI } from "@openzeppelin/contracts/build/contracts/ERC20Permit.json";
+import { ERC20Permit_ABI } from "./ERC20PermitABI";
 import { VersionedTransaction, Transaction } from "@solana/web3.js";
 import MayanForwarderArtifact from "./MayanForwarderArtifact";
 
