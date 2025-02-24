45ad41c2ea0121c73b280e405cfe9ad13219f113
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-02-24 15:24:55 -0500

    m

---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { Suspense, useEffect } from 'react'
+import NavBar from './_components/navbar';
+import { Container } from './_components/container';
+import { useRouter } from 'next/navigation'
+import { DynamicIslandDemo } from '@/components/layout/island';
+import Sidebar, { SidebarSkeleton} from './_components/sidebar';
+
+import { useSession } from "next-auth/react";
+
+const BrowseLayout = ({ 
+    children, 
+}: {
+    children: React.ReactNode;
+}) => {
+
+    const { data: session } = useSession();
+    const router = useRouter();
+  
+    useEffect(() => {
+      if (!session) {
+        const timer = setTimeout(() => {
+          router.push("/");
+        }, 2000);
+  
+        return () => clearTimeout(timer);
+      }
+    }, [session, router]);
+
+  return (
+   
+    <>
+    <NavBar />
+ 
+        <div className="transition-all hidden-scrollbar flex h-full pt-[46px]" >
+        <Suspense fallback={<SidebarSkeleton />}>
+                <Sidebar />
+            </Suspense>
+              
+              
+            <Container>
+                {children}
+            </Container>
+        </ div>
+    </>
+   
+  )
+}
+
+export default BrowseLayout
