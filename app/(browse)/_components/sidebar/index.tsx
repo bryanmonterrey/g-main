ecace3820b8cdc93b8b97a3e40afdbf02eff92fc
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-02-24 17:59:42 -0500

    use client sidebar update

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,39 +1,58 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import { Wrapper } from './wrapper';
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 
 import { Following, FollowingSkeleton } from './following';
 import Recommended, { RecommendedSkeleton } from './recommended';
 
-
-
 import { getRecommended } from '@/lib/recommended-service';
 import { getFollowedUsers } from '@/lib/follow-service';
 
-const Sidebar = async () => {
-    const session = await useSession();
-   // Pass the session to your data fetching functions
-   const [recommended, following] = await Promise.all([
-    getRecommended(session),
-    getFollowedUsers(session)
-]);
+const Sidebar = () => {
+    const { data: session } = useSession();
+    const [recommended, setRecommended] = useState([]);
+    const [following, setFollowing] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const [recommendedData, followingData] = await Promise.all([
+                    getRecommended(session),
+                    getFollowedUsers()
+                ]);
+                
+                setRecommended(recommendedData);
+                setFollowing(followingData);
+            } catch (error) {
+                console.error("Error fetching sidebar data:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        
+        fetchData();
+    }, [session]);
 
-  return (
-    <Wrapper>
-        <div className='flex justify-end'>
-             
-        </div>
+    if (isLoading) {
+        return <SidebarSkeleton />;
+    }
 
-        <div className='space-y-3 pt-2 mt-2'>
-            <Following data={following} />
-            <Recommended data={recommended} />
-        </div>
-    </Wrapper>
-  )
+    return (
+        <Wrapper>
+            <div className='flex justify-end'></div>
+            <div className='space-y-3 pt-2 mt-2'>
+                <Following data={following} />
+                <Recommended data={recommended} />
+            </div>
+        </Wrapper>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
 
 export const SidebarSkeleton = () => {
     return (
