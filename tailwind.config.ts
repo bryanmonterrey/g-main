80129be1a49c3f916700d97c9afeb95aa30d8048
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-03-02 04:03:52 -0500

    w

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,7 +46,7 @@ export default {
 			sleek: '#11111A',
 			buttongray: "#232328",
 			leaf: "#00ED89",
-			azul: "#5643FF",
+			azul: "#5865F2",
 			satin: "#1B1B29",
   			chart: {
   				'1': 'hsl(var(--chart-1))',
