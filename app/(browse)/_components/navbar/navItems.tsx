4023310403196378087a84bdbd26cad2c9ef8608
bry0306 <37124301+bry0306@users.noreply.github.com>
2025-03-04 13:10:55 -0500

    m

--- app/(browse)/_components/navbar/navItems.tsx
+++ app/(browse)/_components/navbar/navItems.tsx
@@ -71,19 +71,19 @@ export const NavItems: React.FC<NavItemsProps> = ({
             key={item.key}
             className={cn(
               'bg-transparent shadow-none hover:bg-transparent transition-all ease-in-out duration-300',
-              pathname === item.path ? 'text-white active:text-juul hover:text-juul' : 'text-white/75 hover:text-white'
+              pathname === item.path ? 'text-white/95 active:text-white hover:text-white' : 'text-white/75 hover:text-white'
             )}
             onClick={() => handleNavigation(item.path)}
           >
-            <div className="h-full flex items-center justify-center gap-1 transition-all ease-in-out duration-300 font-semibold text-sm active:text-juul">
+            <div className="h-full flex items-center justify-center gap-1 transition-all ease-in-out duration-300 font-semibold text-sm active:text-white">
               {item.icon}
               <p>{item.label}</p>
             </div>
           </Button>
         )
       ))}
-      <Button className='bg-transparent shadow-none hover:bg-transparent transition-all ease-in-out duration-300 '>
-        <Search className='w-4 h-4' strokeWidth={2.25} />
+      <Button className=' rounded-full bg-transparent p-2 px-2.5 shadow-none hover:bg-white/5 transition-all ease-in-out duration-300 '>
+        <Search className='w-4 h-4 text-white/75 hover:text-white' strokeWidth={2.50} />
       </Button>
     </>
   );
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -20,7 +20,7 @@ export const Wrapper = ({
 
     return (
         <motion.aside 
-            className="w-[44px] fixed left-0 flex flex-col h-full z-50"      
+            className="w-[44px] fixed left-0 flex flex-col h-[calc(100vh-46px)] z-50 overflow-y-auto"      
             transition={{ duration: 0.25, ease: "easeInOut", spring: 0.5 }}
         >
             {children}
